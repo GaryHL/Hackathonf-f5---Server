bee78578eeb153650514d912a7588a2478516cb4
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-22 13:42:26 +0100

    create boook controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $book = new Book();
+        $book->title = $request->title;
+        $book->description = $request->description;
+        $book->user_id = auth()->id(); // Asocia el ID del usuario autenticado con el libro
+        $book->save();
+    
+        return response()->json($book, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
